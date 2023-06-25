8884cc32d6a6f985b507df5ad1dc31f42947ce33
xiaosongfu <xiaosong.fu@outlook.com>
2023-06-25 15:33:59 +0800

    feat: use `Sign in with Ethereum` spec

--- cmd/apiserver.go
+++ cmd/apiserver.go
@@ -96,7 +96,10 @@ func main() {
 	{
 		// user routers
 		userGroup := v1.Group("/user")
+		userGroup.POST("/refresh_nonce", user.RefreshNonce)
+		userGroup.GET("/retrieve_nonce", user.RetrieveNonce)
 		userGroup.POST("/login", user.Login)
+		userGroup.POST("/login2", user.Login2)
 		userGroup.GET("/users", user.Users)
 		userGroup.GET("/casbin", user.GetFrontendPermission)
 
