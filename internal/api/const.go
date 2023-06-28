d2c61de5e7cf8c92c11875978edbacf33ba14400
hzmangel <hzmangel@gmail.com>
2023-06-28 09:53:14 +0800

    update headers info

--- internal/api/const.go
+++ internal/api/const.go
@@ -3,6 +3,6 @@ package api
 const DefaultPageSize = 10
 
 var ApplicationUploadTemplateHeader = map[string]string{
-	"zh": "1,2,3,4,5",
-	"en": "1,2,3,4,5",
+	"zh": "时间,钱包地址,登记积分,登记Token,事项内容,预算来源,备注,状态,登记人,审核人",
+	"en": "Time,Address,Add Points,Add Token,Content,Budget Source,Note,State,Operator,Auditor",
 }
