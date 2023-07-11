028cb0a0adf963f07348ce4a417e9c326280b893
hzmangel <hzmangel@gmail.com>
2023-07-11 15:16:20 +0800

    Fix model def error

--- internal/model/guild_budget.go
+++ internal/model/guild_budget.go
@@ -12,10 +12,10 @@ type GuildBudget struct {
 	ID           uint            `json:"id" gorm:"primaryKey"`
 	GuildID      uint            `json:"guild_id"` // guild_id
 	Name         string          `json:"name"`
-	Type         BudgetType      `json:"type"`                                   // budget type, credit or token
-	TotalAmount  decimal.Decimal `json:"total_amount" sql:"type:decimal(20,8);"` // total_amount
-	UsedAmount   decimal.Decimal `json:"used_amount" sql:"type:decimal(20,8);"`  // used_amount
-	RemainAmount decimal.Decimal `json:"remain_amount sql:"type:decimal(20,8);"` // remain_amount
+	Type         BudgetType      `json:"type"`                                    // budget type, credit or token
+	TotalAmount  decimal.Decimal `json:"total_amount" sql:"type:decimal(20,8);"`  // total_amount
+	UsedAmount   decimal.Decimal `json:"used_amount" sql:"type:decimal(20,8);"`   // used_amount
+	RemainAmount decimal.Decimal `json:"remain_amount" sql:"type:decimal(20,8);"` // remain_amount
 	CreatedAt    time.Time       `json:"created_at"`
 	UpdatedAt    time.Time       `json:"updated_at"`
 }
