dbf062afddd1b1e673873f6491c51ff3ca155792
hzmangel <hzmangel@gmail.com>
2023-11-05 02:54:23 +0800

    Add idx field to seasons table for simplifier calc scores

--- internal/model/season.go
+++ internal/model/season.go
@@ -7,6 +7,9 @@ type Season struct {
 
 	Name string `gorm:"uniqueIndex size:32"`
 
+	// Numeric index for the season, will be used to calculate latest credits in current season
+	Idx uint `gorm:"uniqueIndex size:32"`
+
 	// TODO: This field do not contains timezone info, need to review code about this
 	StartAt time.Time `gorm:"index"`
 	EndAt   time.Time
