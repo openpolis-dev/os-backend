01f377048b32586092aff4b9e0b8df729db6b250
hzmangel <hzmangel@gmail.com>
2024-01-05 01:37:03 +0800

    Update component_name field name to match others

--- internal/api/component/req_n_resp.go
+++ internal/api/component/req_n_resp.go
@@ -5,7 +5,7 @@ package component
 type ComponentInstance struct {
 	ID            uint   `json:"id"`
 	ComponentId   uint   `json:"component_id"`
-	ComponentName string `json:"component_name"`
+	ComponentName string `json:"name"`
 	Schema        string `json:"schema"`
 	Data          string `json:"data"`
 
