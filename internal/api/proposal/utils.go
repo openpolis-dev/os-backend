9fa2c51fe07e02d4ca27e7e8418739063cb504e1
hzmangel <hzmangel@gmail.com>
2024-01-12 00:05:42 +0800

    New widget_data type passed_proposal

--- internal/api/proposal/utils.go
+++ internal/api/proposal/utils.go
@@ -11,6 +11,23 @@ import (
 	"gorm.io/gorm"
 )
 
+const ListProposalsSQL = `
+SELECT p.id,
+       p.title,
+       lower(p.applicant) as applicant,
+       u.avatar as applicant_avatar,
+       pc.name  as category_name,
+       p.create_ts,
+       p.version,
+       p.state as state_id
+FROM proposals p
+         JOIN (SELECT proposal_record_id, MAX(version) AS max_version
+               FROM proposals
+               GROUP BY proposal_record_id) t2
+              ON p.proposal_record_id = t2.proposal_record_id AND p.version = t2.max_version
+         JOIN proposal_categories pc ON p.proposal_category_id = pc.id
+         JOIN users u ON p.applicant = u.wallet`
+
 const QueryMetaforoUserWithOsUserBaseSQL = `
 SELECT u.wallet            AS wallet,
        mu.metaforo_user_id AS metaforo_user_id,
