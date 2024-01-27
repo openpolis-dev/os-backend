3f1aca66eefc1d8af072d8e375e817031ee4e8c6
hzmangel <hzmangel@gmail.com>
2024-01-27 19:03:10 +0800

    Fix is_hidden filter not working error

--- internal/api/proposal/component.go
+++ internal/api/proposal/component.go
@@ -28,7 +28,9 @@ type ComponentResponse struct {
 func ListComponents(ctx *gin.Context) {
 	db := api.ForContextOnlyDB(ctx)
 	var records []*ComponentResponse
-	err := db.Model(&model.ProposalComponent{}).Where(model.ProposalComponent{IsHidden: false}).Find(&records).Error
+	err := db.Model(&model.ProposalComponent{}).
+		Where("is_hidden = ?", false).
+		Find(&records).Error
 	if err != nil {
 		sdk.LogServerErrorToSentry(ctx, err)
 		ctx.JSON(500, api.ServerError(errors.New("list components failed")))
