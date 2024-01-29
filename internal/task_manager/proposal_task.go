28c33c5b791ab795671bbfb3da32ce5bb90b24bb
hzmangel <hzmangel@gmail.com>
2024-01-29 14:31:27 +0800

    Veto proposal task

--- internal/task_manager/proposal_task.go
+++ internal/task_manager/proposal_task.go
@@ -1,12 +1,37 @@
 package task_manager
 
 import (
+	"encoding/json"
+
 	"github.com/rs/zerolog/log"
 	"github.com/theseed-labs/os-backend/internal/model"
 	"gorm.io/gorm"
 )
 
-func VetoProposal(db *gorm.DB, job *model.CronJob, jobParams string) {
+const queryCronJobRecordFromProposalIdSQL = `select
+	cj.*,
+	p.id as proposal_id
+from
+	cron_jobs cj
+inner join proposal_component_records pcr on	cj.proposal_component_record_id = pcr.id
+inner join proposals p on pcr.proposal_id = p.id
+where p.id = ?`
+
+type VoteProposalParams struct {
+	Applicant            string `json:"applicant"`
+	VetoProposalId       string `json:"proposal_id"`
+	BeVetoedProposalInfo struct {
+		Applicant            string `json:"applicant"`
+		ApplicantAvatar      string `json:"applicant_avatar"`
+		CreateTs             int    `json:"create_ts"`
+		Id                   int    `json:"id"`
+		Name                 string `json:"name"`
+		ProposalCategoryName string `json:"proposal_category_name"`
+		ProposalState        string `json:"proposal_state"`
+	} `json:"proposal_info"`
+}
+
+func CreateVetoProposalTask(db *gorm.DB, job *model.CronJob, jobParams string) {
 	log.Debug().Msgf("start create project task: %+v", job)
 	err := db.Model(&job).Updates(model.CronJob{State: model.CronJobStateRunning}).Error
 	if err != nil {
@@ -17,6 +42,38 @@ func VetoProposal(db *gorm.DB, job *model.CronJob, jobParams string) {
 	execResult := ""
 	jobFailed := false
 
+	var params VoteProposalParams
+	err = json.Unmarshal([]byte(jobParams), &params)
+	if err != nil {
+		log.Warn().Msgf("veto proposal job params error: %+v", err)
+		execResult = err.Error()
+		jobFailed = true
+	} else {
+		// Veto proposal
+		// 1. Mark cronjob related to the specified proposal to terminated
+		// 2. Mark the proposal to vetoed state
+		var proposalTasks []*model.CronJob
+		err := db.Raw(queryCronJobRecordFromProposalIdSQL, params.BeVetoedProposalInfo.Id).Find(&proposalTasks).Error
+		if err != nil {
+			log.Warn().Msgf("fetch be vetoed proposal data error: %+v", err)
+			execResult = err.Error()
+			jobFailed = true
+		} else {
+			for _, r := range proposalTasks {
+				r.State = model.CronJobStateTerminated
+				r.UpdateTs = model.GetCurrentUtcEpochSecond()
+				db.Updates(r)
+			}
+			db.Updates(&proposalTasks)
+
+			err = db.Model(&model.Proposal{}).Update("state = ?", model.ProposalStateVetoed).Error
+			if err != nil {
+				log.Warn().Msgf("fetch be vetoed proposal data error: %+v", err)
+				execResult = err.Error()
+				jobFailed = true
+			}
+		}
+	}
 	job.LastExecTs = model.GetCurrentUtcEpochSecond()
 	job.LastExecResult = execResult
 	job.LastExecutionFailed = jobFailed
