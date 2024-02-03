38cd803047171c1059846ac3bcdf57d606d2f95b
hzmangel <hzmangel@gmail.com>
2024-02-04 02:46:12 +0800

    Add cronjob to set proposal state to executed for no vote proposals

--- internal/api/proposal/proposal.go
+++ internal/api/proposal/proposal.go
@@ -658,7 +658,7 @@ func updateProposalState(db *gorm.DB, user *middleware.CurUser, proposalStrId st
 					proposalRecord.State = int(model.ProposalStateExecuted)
 				} else {
 					proposalRecord.State = int(model.ProposalStatePendingExecution)
-					// TODO: Add cronjob to update the proposal state
+					createProposalAutomationTasks(db, proposalRecord, model.ProposalStateExecuted, "", model.ProposalVoteTypeNone)
 				}
 			} else {
 				for _, record := range voteRecords {
