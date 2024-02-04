1366fc37abc267ea7bed902c3a26d3f960680d7a
hzmangel <hzmangel@gmail.com>
2024-02-04 18:30:51 +0800

    Return ordered components in both auth and non-auth list template API

--- internal/api/proposal/template.go
+++ internal/api/proposal/template.go
@@ -93,14 +93,7 @@ func ListTemplates(ctx *gin.Context) {
 		}
 		return &TemplateResponseWithComponents{
 			TemplateResponse: tmplRsp,
-			Components: lo.Map(r.Components, func(c *model.ProposalComponent, _ int) *ComponentResponse {
-				return &ComponentResponse{
-					ID:            c.ID,
-					Name:          c.Name,
-					Schema:        c.Schema,
-					ScreenshotUri: c.ScreenshotUri,
-				}
-			}),
+			Components:       getTemplateComponents(r),
 		}
 	})
 
@@ -150,32 +143,7 @@ func ListTemplatesWithPerm(ctx *gin.Context) {
 			return rslt && r
 		}, true)
 
-		compNameMapping := lo.SliceToMap(tmplDbRcd.Components, func(c *model.ProposalComponent) (string, *model.ProposalComponent) {
-			return c.Name, c
-		})
-
-		var components []*ComponentResponse
-		if tmplDbRcd.ComponentNameList != nil {
-			for _, name := range tmplDbRcd.ComponentNameList {
-				if comp, ok := compNameMapping[name]; ok {
-					components = append(components, &ComponentResponse{
-						ID:            comp.ID,
-						Name:          comp.Name,
-						Schema:        comp.Schema,
-						ScreenshotUri: comp.Schema,
-					})
-				}
-			}
-		} else {
-			components = lo.Map(tmplDbRcd.Components, func(c *model.ProposalComponent, _ int) *ComponentResponse {
-				return &ComponentResponse{
-					ID:            c.ID,
-					Name:          c.Name,
-					Schema:        c.Schema,
-					ScreenshotUri: c.ScreenshotUri,
-				}
-			})
-		}
+		components := getTemplateComponents(&tmplDbRcd)
 
 		return &TemplateResponseWithComponents{
 			TemplateResponse: *r,
@@ -266,4 +234,34 @@ func UpdateTemplate(ctx *gin.Context) {
 	}
 
 	ctx.JSON(200, api.Success(newTmplRcd))
+}
+
+func getTemplateComponents(tmplDbRcd *model.ProposalTemplate) []*ComponentResponse {
+	compNameMapping := lo.SliceToMap(tmplDbRcd.Components, func(c *model.ProposalComponent) (string, *model.ProposalComponent) {
+		return c.Name, c
+	})
+	var components []*ComponentResponse
+	if tmplDbRcd.ComponentNameList != nil {
+		for _, name := range tmplDbRcd.ComponentNameList {
+			if comp, ok := compNameMapping[name]; ok {
+				components = append(components, &ComponentResponse{
+					ID:            comp.ID,
+					Name:          comp.Name,
+					Schema:        comp.Schema,
+					ScreenshotUri: comp.Schema,
+				})
+			}
+		}
+	} else {
+		components = lo.Map(tmplDbRcd.Components, func(c *model.ProposalComponent, _ int) *ComponentResponse {
+			return &ComponentResponse{
+				ID:            c.ID,
+				Name:          c.Name,
+				Schema:        c.Schema,
+				ScreenshotUri: c.ScreenshotUri,
+			}
+		})
+	}
+
+	return components
 }
