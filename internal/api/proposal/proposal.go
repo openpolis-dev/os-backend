09d17b47812a23f2f8b912fc3b8d162b39ae1777
hzmangel <hzmangel@gmail.com>
2024-02-06 01:02:25 +0800

    Add new cron_job to update no vote proposal to executed

--- internal/api/proposal/proposal.go
+++ internal/api/proposal/proposal.go
@@ -224,6 +224,12 @@ func Update(ctx *gin.Context) {
 				ctx.JSON(http.StatusInternalServerError, api.ServerError(errors.New("create proposal error")))
 				return
 			}
+			if err = createJobToUpdateNoVoteProposalToNextState(db, proposalRecord, proposalRecord.CreateTs+proposalRecord.PublicitySecond+proposalRecord.PendingExecutionSecond, model.ProposalStateExecuted); err != nil {
+				log.Error().Msgf("create proposal state change error: %+v", err)
+				sdk.LogServerErrorToSentry(ctx, err)
+				ctx.JSON(http.StatusInternalServerError, api.ServerError(errors.New("create proposal error")))
+				return
+			}
 		}
 	}
 
