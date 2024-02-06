ead6026143d43ef5b68cc1f36e5ff0918dbc9c56
Soul_Cai <5113047+CooCoode@users.noreply.github.com>
2024-02-07 00:05:32 +0800

    fix bug update project overlink to change status

--- internal/api/project/project.go
+++ internal/api/project/project.go
@@ -299,6 +299,7 @@ func Update(ctx *gin.Context) {
 	}
 
 	//  check permission
+	overProject := false
 	if (len(req.OverLink) > 0) || (len(req.Sponsors) > 0) {
 		ok, err := enforcer.HasRoleForUser(common.FormatUserWallet(user.Wallet), internal.RoleHall)
 		if err != nil {
@@ -322,7 +323,7 @@ func Update(ctx *gin.Context) {
 		proj.Sponsors = sponsors
 		proj.OverLink = req.OverLink
 		if len(req.OverLink) > 0 {
-			proj.Status = model.ProjectStatusClosed
+			overProject = true
 		}
 	} else {
 		ok, err := enforcer.HasRoleForUser(common.FormatUserWallet(user.Wallet), internal.RoleHall)
@@ -369,6 +370,10 @@ func Update(ctx *gin.Context) {
 	proj.Desc = req.Desc
 	proj.ContantWay = req.ContantWay
 	proj.OfficialLink = req.OfficialLink
+
+	if overProject {
+		proj.Status = model.ProjectStatusClosed
+	}
 
 	proj.UpdateTs = model.GetCurrentUtcEpochSecond()
 	proj.UpdatedAt = time.Now().In(internal.ProjectTimezone)
