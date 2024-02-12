05d383acbde9cc1eb9de1121ba425e4d2149615b
hzmangel <hzmangel@gmail.com>
2024-02-12 22:11:21 +0800

    Update system variable table struct

--- internal/model/system_variable.go
+++ internal/model/system_variable.go
@@ -6,20 +6,21 @@ import (
 )
 
 type SystemVariable struct {
-	NextSip    int
-	AdminToken string
+	Name     string
+	NumValue int
+	StrValue string
 }
 
 func GetNextSipValue(db *gorm.DB) (int, error) {
 	log.Debug().Msgf("get next sip value request")
 	var sipValue []int
 	err := db.Transaction(func(tx *gorm.DB) error {
-		err := tx.Model(&SystemVariable{}).Where("1=1").Update("next_sip", gorm.Expr("next_sip + ?", 1)).Error
+		err := tx.Model(&SystemVariable{}).Where("name='sip'").Update("num_value", gorm.Expr("num_value + ?", 1)).Error
 		if err != nil {
 			log.Error().Msgf("update sip value error: %+v", err)
 			return err
 		}
-		if err = tx.Model(&SystemVariable{}).Limit(1).Pluck("next_sip", &sipValue).Error; err != nil {
+		if err = tx.Model(&SystemVariable{}).Limit(1).Pluck("num_value", &sipValue).Error; err != nil {
 			log.Error().Msgf("update sip value error: %+v", err)
 			return err
 		}
