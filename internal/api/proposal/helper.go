2dad5af2385effd203e1dd25006afa37fbf0d37a
hzmangel <hzmangel@gmail.com>
2024-07-12 10:27:50 +0800

    Change to use original start time

--- internal/api/proposal/helper.go
+++ internal/api/proposal/helper.go
@@ -668,8 +668,8 @@ func SaveProposalToMetaforo(db *gorm.DB, origProposalRecordId uint, voteType int
 
 		// Get vote record from original record and update the timestamp
 		// The updated vote record will be saved by response in GetProposal function
-		voteStartTime := time.Unix(updatedProposalRecord.CreateTs, 0).Add(origProposalRecord.PublicityDuration())
-		voteEndTime := time.Unix(updatedProposalRecord.CreateTs, 0).Add(origProposalRecord.PublicityDuration() + origProposalRecord.VoteDuration())
+		voteStartTime := time.Unix(origProposalRecord.CreateTs, 0).Add(origProposalRecord.PublicityDuration())
+		voteEndTime := time.Unix(origProposalRecord.CreateTs, 0).Add(origProposalRecord.PublicityDuration() + origProposalRecord.VoteDuration())
 		log.Debug().Msgf("Resubmit withdraw propoesal, vote start time: %s, vote end time: %s", voteStartTime.Format(time.RFC3339), voteEndTime.Format(time.RFC3339))
 
 		if voteStartTime.Before(time.Now().UTC()) {
