d5bcfecdaaac2a6112b3c2091684c6b8eac7fea0
hzmangel <hzmangel@gmail.com>
2024-10-20 14:08:29 +0800

    Remove unused API

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -281,7 +281,6 @@ func setupRouter(cfg *config.Config, db *gorm.DB, enforcer *casbin.SyncedEnforce
 		// TODO: Move to authorized group
 		dataSrv := v1.Group("/data_srv")
 		dataSrv.GET("/aggr_scr", data_srv.AggrScr)
-		dataSrv.POST("/fetch_mf_mint_data", data_srv.FetchCurrentSeasonMetaforoMintData)
 
 		// Proposal component routers
 		componentRouter := v1.Group("/proposal_components")
