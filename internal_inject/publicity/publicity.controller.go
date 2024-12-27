92310b8e5d3d79f4a8aa0388f4b10af0c9fafb28
Soul_Cai <5113047+CooCoode@users.noreply.github.com>
2024-12-27 22:54:46 +0800

    fix: sort key

--- internal_inject/publicity/publicity.controller.go
+++ internal_inject/publicity/publicity.controller.go
@@ -147,7 +147,7 @@ func (c *PublicityController) List(ctx *gin.Context) {
 		return
 	}
 
-	sortKey := "p.create_at asc, p.update_at"
+	sortKey := "p.update_at"
 	order := "desc"
 
 	data, err := model.QueryRows[PublicityInfo](querySeg, &gormfind.Page{
