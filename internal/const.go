bcdf946291be7bea44bb411a90360ecdf78ec70a
hzmangel <hzmangel@gmail.com>
2025-08-18 23:37:13 +0800

    feat(wallet): add burn address validation in wallet utils
    Add BurnAddress constant and validate against it in wallet address validation to prevent using the burn address as a valid wallet.

--- internal/const.go
+++ internal/const.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/samber/lo"
 )
 
@@ -34,6 +35,8 @@ const SeedContractAddr = "0x30093266E34a816a53e302bE3e59a93B52792FD4"
 
 const EnsoulSbtContractType = "erc1155"
 const EnsoulSbtContractAddr = "0x9d34D407D8586478b3e4c39BE633ED3D7be1c80C"
+
+var BurnAddress = common.HexToAddress("0x0000000000000000000000000000000000000000")
 
 const CityHallTokenId = "70"
 
